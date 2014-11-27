ca3c92317b5a370d48614e7900f7164d253c4546
Patrick Wilson <pdubslax@umich.edu>
2014-11-26 19:18:08 -0500

    fully automated

--- bball.py
+++ bball.py
@@ -8,11 +8,57 @@ for line in f2:
 	wordList = re.sub("[^\S]", " ",  line).split()
 	listOfRPI[wordList[0]]=wordList[1]
 
+from mechanize import Browser
+from BeautifulSoup import BeautifulSoup
+
+mech = Browser()
+mech2 = Browser()
+
+url = "http://www.masseyratings.com/scores.php?s=267615&sub=11590&all=1&mode=2&format=0"
+page = mech.open(url)
+
+html = page.read()
+soup = BeautifulSoup(html)
+table = soup.find("pre")
+
+
+url2 = "http://realtimerpi.com/rpi_Men.html"
+page2 = mech2.open(url2)
+html2 = page2.read()
+soup2 = BeautifulSoup(html2)
+table2 = soup2.findChildren("table")
+dataTable = table2[4]
+rows = dataTable.findChildren(['th', 'tr'])
+firstTime = True
+
+test=open('rpi2.txt','w')
+
+for row in rows:
+	cells = row.findChildren('a')
+	start = 0
+	for cell in cells:
+		if len(cells) != 4:
+			break
+		value = cell.string
+		if (start==0):
+			#this is the teamname
+			newName = value.replace(" ", "")+';'
+			test.write(newName)
+
+		elif (start==1):
+			#the good rpi
+			newRPI = value+';'+'\n'
+			test.write(newRPI)
+		start += 1 
+
+test.close()
+
 f3 = open('rpi2.txt', 'rw')
 listOfRPI2 = {}
 for line in f3:
 	wordList = re.sub(";", " ",  line).split()
 	listOfRPI2[wordList[0]]=float(wordList[1])
+
 
 
 def dominanceScore(opposingTeam, MOV, status):
@@ -30,12 +76,27 @@ def dominanceScore(opposingTeam, MOV, status):
 		finalResult = MOV*(rpi**3)*mult
 	return finalResult
 
-f = open('bball.txt', 'rw')
+
+
+test=open('test.txt','w')
+test.write(table.string)
+test.close()
+
+
+with open("test.txt") as infile, open("test2.txt", "w") as outfile:
+    for line in infile:
+        outfile.write(line.replace(";", ""))
+outfile.close()
+
+f = open('test2.txt', 'rw')
+
 
 teamDic = {}
 
-for line in f:
+for line in f:	
 	wordList = re.sub("[^\S]", " ",  line).split()
+	if len(wordList)==0:
+		break
 	homeTeam = ""
 	awayTeam = ""
 	foundHomeTeam = False
@@ -78,7 +139,6 @@ for line in f:
 			awayTeam += wordList[i]
 			i+=1
 		awayScore = int(wordList[i])
-	#print homeTeam + ' ' + str(homeScore) + ' ' + awayTeam + ' ' + str(awayScore)
 #status 1=home 2 =away 0= neutral	
 	if (homeTeam in teamDic):
 		if (neutralSite):
